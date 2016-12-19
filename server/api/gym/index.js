d4d3ae5620c98fa2ac4972da4ae849ad04ffa3a9
nipun thathsara <nipun thathsara>
2016-12-19 19:21:33 +0530

    All APIs protected

--- server/api/gym/index.js
+++ server/api/gym/index.js
@@ -4,16 +4,18 @@ var express = require('express');
 var multer = require('multer');
 var controller = require('./gym.controller');
 
+var auth = require('../../auth/auth.service');
+
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.get('/findTen/:id', controller.findTen);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/:id',auth.isAuthenticated(), controller.show);
+router.get('/findTen/:id', controller.findTen);//remove this route
 
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
 //******nipun delte route
-router.delete('/:id', controller.delete);//send id in params
+router.delete('/:id', auth.isAuthenticated(), controller.delete);//send id in params
 
 module.exports = router;
--- server/api/trainer/index.js
+++ server/api/trainer/index.js
@@ -3,15 +3,15 @@
 var express = require('express');
 var multer = require('multer');
 var controller = require('./trainer.controller');
-
+var auth = require('../../auth/auth.service');
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
 //******nipun delte route
-router.delete('/:id', controller.delete);//send id in params
+router.delete('/:id', auth.isAuthenticated(), controller.delete);//send id in params
 
 module.exports = router;
