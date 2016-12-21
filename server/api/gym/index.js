8e84a9dc7aebf0821aee126de1b7bf87d5bf109e
Thushila <thushila.91@gmail.com>
2016-12-21 08:51:01 -0800

    removed authentication

--- server/api/gym/index.js
+++ server/api/gym/index.js
@@ -8,13 +8,20 @@ var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', auth.isAuthenticated(), controller.index);
-router.get('/:id',auth.isAuthenticated(), controller.show);
+// router.get('/', auth.isAuthenticated(), controller.index);
+// router.get('/:id',auth.isAuthenticated(), controller.show);
+// router.get('/:id',auth.isAuthenticated(), controller.show);
+router.get('/',  controller.index);
+router.get('/:id', controller.show);
+router.get('/:id', controller.show);
 router.get('/findTen/:id', controller.findTen);//remove this route
 
-router.post('/', auth.isAuthenticated(), controller.create);
-router.put('/:id', auth.isAuthenticated(), controller.update);
-router.patch('/:id', auth.isAuthenticated(), controller.update);
+// router.post('/', auth.isAuthenticated(), controller.create);
+// router.put('/:id', auth.isAuthenticated(), controller.update);
+// router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.post('/', controller.create);
+router.put('/:id', controller.update);
+router.patch('/:id', controller.update);
 //******nipun delte route
 router.delete('/:id', auth.isAuthenticated(), controller.delete);//send id in params
 
--- server/api/trainer/index.js
+++ server/api/trainer/index.js
@@ -6,12 +6,19 @@ var controller = require('./trainer.controller');
 var auth = require('../../auth/auth.service');
 var router = express.Router();
 
-router.get('/', auth.isAuthenticated(), controller.index);
-router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/', auth.isAuthenticated(), controller.create);
-router.put('/:id', auth.isAuthenticated(), controller.update);
-router.patch('/:id', auth.isAuthenticated(), controller.update);
-//******nipun delte route
-router.delete('/:id', auth.isAuthenticated(), controller.delete);//send id in params
+// router.get('/', auth.isAuthenticated(), controller.index);
+// router.get('/:id', auth.isAuthenticated(), controller.show);
+// router.post('/', auth.isAuthenticated(), controller.create);
+// router.put('/:id', auth.isAuthenticated(), controller.update);
+// router.patch('/:id', auth.isAuthenticated(), controller.update);
+// //******nipun delte route
+// router.delete('/:id', auth.isAuthenticated(), controller.delete);//send id in params
 
+router.get('/', controller.index);
+router.get('/:id', controller.show);
+router.post('/', controller.create);
+router.put('/:id', controller.update);
+router.patch('/:id', controller.update);
+//******nipun delte route
+router.delete('/:id', controller.delete);
 module.exports = router;
